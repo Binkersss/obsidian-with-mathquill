2900b594771df14384995bbadf10ef362663438e
Nathaniel Chappelle <nathaniel.chappelle@proton.me>
2024-10-11 10:38:16 -0700

    Mathquill Command Added, no testing

--- main.ts
+++ main.ts
@@ -1,69 +1,47 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import "./mathquill.js";
+import "./mathquill.css"
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface MathQuillPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: MathQuillPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MathQuillPlugin extends Plugin {
+	settings: MathQuillPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		console.log('Mathquill Plugin loaded');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'insert-mathquill',
+			name: 'Insert Mathquill Equation',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				const container = this.createMathQuillContainer();
+				document.body.appendChild(container);
+
+				const MQ = (window as any).MathQuill.getInterface(2); // Get Mathquill interface
+				const mathField = MQ.MathField(container, {
+					spaceBeahveLiveTab: true;
+					handles: {
+						edit: () => {
+							const enteredMath = mathField.latex();
+							// Inject LaTeX to editor
+							editor.replaceSelection('$$${enteredmath$$$');
+							document.body.removeChild(container); // Remove input field after use
+						}
+					}
+				})
 			}
-		});
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
@@ -79,7 +57,23 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {
+		console.log('MathQuill Plugin unloaded');
+	}
 
+	createMathQuillContainer() {
+		const container = document.createElement('div');
+		container.id = 'math-field';
+		container.style.position = 'fixed';
+		container.style.top = '50%';
+		container.style.left = '50%';
+		container.style.transform = 'translate(-50%, -50%)';
+		container.style.zIndex = '1000';
+		container.style.backgroundColor = 'white';
+		container.style.border = '1px solid black';
+		container.style.padding = '10px';
+		container.style.width = '400px';
+		container.style.height = '200px';
+		return container;
 	}
 
 	async loadSettings() {
